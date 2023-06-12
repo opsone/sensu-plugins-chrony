b9fa88a2b3f8777c102c3332ea7342c71134489a
Kevyn Lebouille <kevyn.lebouille@opsone.net>
2023-06-12 09:56:14 +0200

    Version 0.1.0

---
+++ bin/check-chrony.rb
@@ -0,0 +1,142 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'sensu-plugin/check/cli'
+
+class CheckChrony < Sensu::Plugin::Check::CLI
+  option :chronyc_cmd,
+         description: 'Path to chronyc executable (default: /usr/bin/chronyc)',
+         short: '-c <PATH>',
+         long: '--chronyc-cmd <PATH>',
+         default: '/usr/bin/chronyc'
+
+  option :warn_offset,
+         description: 'Warn if OFFSET exceeds current offset (ms)',
+         long: '--warn-offset <OFFSET>',
+         proc: proc(&:to_f),
+         default: 50
+
+  option :crit_offset,
+         description: 'Critical if OFFSET exceeds current offset (ms)',
+         long: '--crit-offset <OFFSET>',
+         proc: proc(&:to_f),
+         default: 100
+
+  option :warn_stratum,
+         description: 'Warn if STRATUM exceeds current stratum',
+         long: '--warn-stratum <STRATUM>',
+         proc: proc(&:to_i),
+         default: 10
+
+  option :crit_stratum,
+         description: 'Critical if STRATUM exceeds current stratum',
+         long: '--crit-stratum <STRATUM>',
+         proc: proc(&:to_i),
+         default: 16
+
+  def run
+    stratum = nil
+    offset = nil
+    status = nil
+
+    `#{config[:chronyc_cmd]} tracking`.each_line do |line|
+      case line.downcase
+      when /^stratum\s*:\s*(\d+)$/
+        stratum = Regexp.last_match(1).to_i
+      when /^last offset\s*:\s*([\-\+]?[.\d]+)\s*seconds$/
+        # convert from seconds to milliseconds
+        offset = Regexp.last_match(1).to_f * 1000
+      when /^leap status\s*:\s*(.*?)$/
+        status = Regexp.last_match(1)
+      end
+    end
+
+    if stratum
+      msg = "NTP stratum is #{stratum}"
+
+      if stratum >= config[:crit_stratum]
+        msg += ", expected < #{config[:crit_stratum]}"
+        critical msg
+      elsif stratum >= config[:warn_stratum]
+        msg += ", expected < #{config[:warn_stratum]}"
+        warning msg
+      else
+        ok msg
+      end
+    else
+      unknown 'Failed to look up NTP stratum'
+    end
+
+    if offset
+      msg = "NTP offset is #{offset.round(4)}ms"
+
+      if offset >= config[:crit_offset] || offset <= -config[:crit_offset]
+        msg += ", expected > -#{config[:crit_offset]} and < #{config[:crit_offset]}"
+        critical msg
+      elsif offset >= config[:warn_offset] || offset < -config[:warn_offset]
+        msg += ", expected > -#{config[:warn_offset]} and < #{config[:warn_offset]}"
+        warning msg
+      else
+        ok msg
+      end
+    else
+      unknown 'Failed to look up NTP offset'
+    end
+
+    if status
+      msg = "NTP status is '#{status}'"
+
+      if status != 'normal'
+        msg += ', expected \'Normal\''
+        critical msg
+      else
+        ok msg
+      end
+    else
+      unknown 'Failed to look up NTP status'
+    end
+  end
+
+  private
+
+  def send_client_socket(data)
+    sock = UDPSocket.new
+    sock.send("#{data}\n", 0, '127.0.0.1', 3030)
+  end
+
+  def send_ok(check_name, msg)
+    event = {
+      'name' => check_name,
+      'status' => 0,
+      'output' => "#{self.class.name} OK: #{msg}"
+    }
+    send_client_socket(event.to_json)
+  end
+
+  def send_warning(check_name, msg)
+    event = {
+      'name' => check_name,
+      'status' => 1,
+      'output' => "#{self.class.name} WARNING: #{msg}"
+    }
+    send_client_socket(event.to_json)
+  end
+
+  def send_critical(check_name, msg)
+    event = {
+      'name' => check_name,
+      'status' => 2,
+      'output' => "#{self.class.name} CRITICAL: #{msg}"
+    }
+    send_client_socket(event.to_json)
+  end
+
+  def send_unknown(check_name, msg)
+    event = {
+      'name' => check_name,
+      'status' => 3,
+      'output' => "#{self.class.name} UNKNOWN: #{msg}"
+    }
+    send_client_socket(event.to_json)
+  end
+end
---
+++ lib/sensu/plugins/chrony.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require_relative 'chrony/version'
+
+module Sensu
+  module Plugins
+    module Chrony
+      class Error < StandardError; end
+    end
+  end
+end
